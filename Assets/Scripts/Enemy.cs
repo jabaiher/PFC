85ead591bf361987b85ff8c2ed0467d646f2470d
jabaiher <jabaiher@inf.upv.es>
2014-07-14 17:39:24 +0200

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	
+	public Animator anim;
+	public float live = 100.0f;
+	public bool activeChar = true;
+	int hitID = Animator.StringToHash("hit");
+	int runState = Animator.StringToHash("Base Layer.run");
+	int walkState = Animator.StringToHash("Base Layer.walk");
+	
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+		anim.SetFloat ("live", live);
+		anim.SetBool ("active", activeChar);
+	}
+	
+	
+	void Update ()
+	{
+		float move = Input.GetAxis ("Vertical");
+		anim.SetFloat("Speed", move);
+		
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+		if(stateInfo.nameHash == runState)
+		{
+
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		anim.SetTrigger (hitID);
+	}
+
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public GameObject estructure;
+	public float towerHeight;
+	public GameObject character;
+	public string scriptClassPath;
+	public GameObject weapon;
+	// Use this for initialization
+	void Start () {
+		estructure = (GameObject)Instantiate(estructure, transform.position, Quaternion.identity);
+		character = (GameObject)Instantiate(character, new Vector3(transform.position.x,transform.position.y + towerHeight,transform.position.z), Quaternion.identity);
+		character.AddComponent(scriptClassPath);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
